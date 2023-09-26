124161df941e5d2b455fdd20b6314b7e0b5b1ec4
Jordan <jwald3@brockport.edu>
2023-09-26 11:58:24 -0400

    Added CLI key-through menu

--- Program.cs
+++ Program.cs
@@ -1,13 +1,90 @@
-﻿using System;
-using MusicPlaylist.BusinessLogic;
+﻿using MusicPlaylist.BusinessLogic;
 using MusicPlaylist.BusinessLogic.Collections;
 using MusicPlaylist.BusinessLogic.Models;
-using Newtonsoft.Json;
+
 
 namespace MusicPlaylist
 {
     class Program
     {
+        static void DisplayMenu(List<string> items, int selectedItemIndex)
+        {
+            for (int i = 0; i < items.Count; i++)
+            {
+                if (i == selectedItemIndex)
+                {
+                    Console.BackgroundColor = ConsoleColor.Gray;
+                }
+
+                Console.WriteLine(items[i]);
+
+                if (i == selectedItemIndex)
+                {
+                    Console.ResetColor();
+                }
+            }
+        }
+
+        static void HandleSelection(string selectedItem, Playlist playlist)
+        {
+            switch (selectedItem)
+            {
+                case "See all songs":
+                    Console.Clear();
+                    Console.WriteLine("All songs:");
+                    Console.WriteLine();
+                    playlist.PrintSongs();
+                    Console.WriteLine();
+                    Console.WriteLine("Press any key to continue...");
+                    Console.ReadKey();
+                    break;
+                case "Start playlist":
+                    Console.Clear();
+                    Console.WriteLine("Starting playlist...");
+                    Console.WriteLine();
+                    playlist.StartPlaylist();
+                    Console.WriteLine();
+                    Console.WriteLine("Press any key to continue...");
+                    Console.ReadKey();
+                    break;
+                case "Get current song":
+                    Console.Clear();
+                    Console.WriteLine("Current song:");
+                    Console.WriteLine();
+                    Song? song = playlist.GetNowPlaying();
+                    Console.WriteLine(song is null ? "No song is playing" : song.ToString());
+                    Console.WriteLine();
+                    Console.WriteLine("Press any key to continue...");
+                    Console.ReadKey();
+                    break;
+                case "Next song":
+                    Console.Clear();
+                    Console.WriteLine("Next song:");
+                    Console.WriteLine();
+                    playlist.SkipSong();
+                    Song? nextSong = playlist.GetNowPlaying();
+                    Console.WriteLine(nextSong is null ? "No song is playing" : nextSong.ToString());
+                    Console.WriteLine();
+                    Console.WriteLine("Press any key to continue...");
+                    Console.ReadKey();
+                    break;
+                case "Previous song":
+                    Console.Clear();
+                    Console.WriteLine("Previous song:");
+                    Console.WriteLine();
+                    playlist.GoBackSong();
+                    Song? previousSong = playlist.GetNowPlaying();
+                    Console.WriteLine(previousSong is null ? "No song is playing" : previousSong.ToString());
+                    Console.WriteLine();
+                    Console.WriteLine("Press any key to continue...");
+                    Console.ReadKey();
+                    break;
+                case "Exit":
+                    Environment.Exit(0);
+                    break;
+            }
+        }
+
         static void Main(string[] args)
         {
             SongDataLoader songDataLoader = new SongDataLoader();
@@ -20,55 +97,38 @@ namespace MusicPlaylist
                 playlist.AddRange(songs);
             }
 
-
-            char response = new char();
-            System.Console.WriteLine("");
-
-            while (response != 'q')
+            List<string> menuItems = new List<string>
             {
-                Console.WriteLine("Welcome to your MP3 player!");
-                Console.WriteLine("Here are your actions:");
-                Console.WriteLine("a - See all songs");
-                Console.WriteLine("s - Start playlist");
-                Console.WriteLine("p - Get song playing");
-                Console.WriteLine("n - Go forward one song");
-                Console.WriteLine("b - Go back one song");
-                Console.WriteLine("q - Quit the program");
+                "See all songs",
+                "Start playlist",
+                "Get current song",
+                "Next song",
+                "Previous song",
+                "Exit"
+            };
 
-                response = Console.ReadKey().KeyChar;
+            int currentMenuIndex = 0;
 
-                System.Console.WriteLine("");
+            while (true)
+            {
+                Console.Clear();
+                DisplayMenu(menuItems, currentMenuIndex);
 
-                switch(response)
+                ConsoleKeyInfo keyInfo = Console.ReadKey();
+
+                switch (keyInfo.Key)
                 {
-                    case 'a':
-                    case 'A':
-                        playlist.PrintSongs();
+                    case ConsoleKey.UpArrow:
+                        currentMenuIndex = (currentMenuIndex == 0) ? menuItems.Count - 1 : currentMenuIndex - 1;
                         break;
-                    case 's':
-                    case 'S':
-                        playlist.StartPlaylist();
+                    case ConsoleKey.DownArrow:
+                        currentMenuIndex = (currentMenuIndex == menuItems.Count - 1) ? 0 : currentMenuIndex + 1;
                         break;
-                    case 'p':
-                    case 'P':
-                        Song? nowPlaying = playlist.GetNowPlaying();
-                        System.Console.WriteLine(nowPlaying);
+                    case ConsoleKey.Enter:
+                        HandleSelection(menuItems[currentMenuIndex], playlist);
                         break;
-                    case 'n':
-                    case 'N':
-                        playlist.SkipSong();
-                        break;
-                    case 'b':
-                    case 'B':
-                        playlist.GoBackSong();
-                        break;
-                    case 'q':
-                    case 'Q':
-                        break;
-                    default:
-                        continue;
-
                 }
+
             }
 
         }
